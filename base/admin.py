04434995066cf7c1e606c2acb21b120fd0bf123a
Lemmy Mwaura <lemminton@gmail.com>
2022-04-01 21:53:28 +0300

    registered models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post, Comment, Profile, Like
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Like)
