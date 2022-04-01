7511febadcf7dd8ebf0ed81ebd2f8c1865b5e00a
Lemmy Mwaura <lemminton@gmail.com>
2022-04-01 11:55:15 +0300

    Configured and added Templates/Static files settings

--- instagram_clone/settings.py
+++ instagram_clone/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'instagram_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_DIR = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
