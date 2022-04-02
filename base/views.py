37cef6d33c0ad4021b3e9473934ec4bbdfd166cd
Lemmy Mwaura <lemminton@gmail.com>
2022-04-02 10:49:03 +0300

    search functionality implemented

--- base/views.py
+++ base/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Post, Profile, Like, Comment
+from django.db.models import Q
 
 def home(request):
-    posts = Post.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    posts = Post.objects.filter(
+        Q(caption__icontains = q) |
+        Q(image_name__icontains = q) |
+        Q(author__username__icontains = q)
+    )
 
-    context = {'posts': posts}
+    context = { 'posts': posts }
     return render(request, 'base/home.html', context)
 
 def room(request):
