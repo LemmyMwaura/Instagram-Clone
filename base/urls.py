d965c31830cefbc24c20fdb427ec7138f9b98663
Lemmy Mwaura <lemminton@gmail.com>
2022-04-02 13:12:33 +0300

    Implemented delete and create crud operations

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home , name='home'),
+    path('create_post/', views.create_post, name='create-post'),
+    path('update_post/<str:pk>/', views.update_post, name='update-post'),
+    path('deleete_post/<str:pk>/', views.delete_post, name='delete-post')
 ]
