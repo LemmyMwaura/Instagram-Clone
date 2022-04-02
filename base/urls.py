b8f651c060af1ddef3c387f04485fcb4eb9c88ea
Lemmy Mwaura <lemminton@gmail.com>
2022-04-02 13:19:37 +0300

    authentication functionality implemented

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home , name='home'),
     path('create_post/', views.create_post, name='create-post'),
     path('update_post/<str:pk>/', views.update_post, name='update-post'),
-    path('deleete_post/<str:pk>/', views.delete_post, name='delete-post')
+    path('delete_post/<str:pk>/', views.delete_post, name='delete-post'),
+    path('login/', views.login_page, name='login-page'),
+    path('register/', views.register_user, name='register')
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Post, Profile, Like, Comment
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
+from django.contrib import messages 
+from .forms import PostForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -13,6 +19,81 @@ def home(request):
     context = { 'posts': posts }
     return render(request, 'base/home.html', context)
 
-def room(request):
+def login_page(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        messages.success(request, 'Already Logged in')
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist.')
+
+    context={ 'page':page }
+    return render(request, 'base/login_register.html', context)
+
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def register_user(request):
+    page = 'register'
+    form = UserCreationForm
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+
+    context = { 'page':page, 'form':form }
+    return render(request, 'base/login_register.html', context)
+
+@login_required(login_url='login')
+def create_post(request):
+    form = PostForm()
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/post_form.html', context)
+
+@login_required(login_url='login')
+def update_post(request):
     context = {}
-    return render(request, 'base/room.html', context)
+    return render(request, 'base/home.html', context)
+
+@login_required(login_url='login')
+def delete_post(request, pk):
+    post = Post.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        try:
+            post.delete()
+            return redirect('home')
+        except:
+            messages.error('Post does not exist')
+
+    context = { 'obj':post }
+    return render(request, 'base/delete.html', context)
